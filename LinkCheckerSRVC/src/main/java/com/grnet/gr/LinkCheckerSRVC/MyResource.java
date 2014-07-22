82bbebb332c9c570ce5fd213c9d3877e021fcb93
vogias <vogias@admin.grnet.gr>
2014-07-22 12:41:14 +0300

    c

--- LinkCheckerSRVC/src/main/java/com/grnet/gr/LinkCheckerSRVC/MyResource.java
+++ LinkCheckerSRVC/src/main/java/com/grnet/gr/LinkCheckerSRVC/MyResource.java
@@ -3,7 +3,6 @@ package com.grnet.gr.LinkCheckerSRVC;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
